fa7cbd747fa0ac77a0a4a7f506b5bfd3b10dc0a4
DJhuan <jhuancarlossd@proton.me>
2025-05-31 11:42:18 -0300

    Add: compileAndRun now supports debug and counter examples.

--- scriptCompileAndRun.sh
+++ scriptCompileAndRun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Verifica se todos os parâmetros foram passados
-if [ $# -ne 3 ]; then
+if [ $# -lt 3 ]; then
   echo "Uso: $0 <arquivo_do_yacc.y> <arquivo_do_lex.l> <arquivo_de_teste.txt>"
   exit 1
 fi
@@ -12,8 +12,19 @@ ARQ_TESTE="$3"
 
 cd "c-language"
 
+BISON_OPT=""
+for arg in "$@"; do
+  if [ "$arg" = "-d" ]; then
+    echo "Modo debug ativado."
+    BISON_OPT="$BISON_OPT --debug"
+  elif [ "$arg" = "-cep" ]; then
+    echo "Contraexemplos ativados."
+    BISON_OPT="$BISON_OPT -Wcounterexamples"
+  fi
+done
+
 echo "Compilando arquivo .y."
-bison -d -o parser.tab.c c-language.y
+bison $BISON_OPT -d -o parser.tab.c c-language.y
 
 if [ ! -f "parser.tab.c" ]; then
   echo "Erro na compilação do arquivo '.y'!"
