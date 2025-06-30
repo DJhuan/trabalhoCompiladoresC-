6af347a40e86dee84582d5e4ba93e076ce5b68bf
DJhuan <jhuancarlossd@proton.me>
2025-06-29 21:24:55 -0300

    Add: preview TabDeSimbolos

--- c-language/TabDeSimbolos.c
+++ c-language/TabDeSimbolos.c
@@ -1,27 +1,163 @@
 #include "TabDeSimbolos.h"
 #include <stdlib.h>
+#include <string.h>
 
-TabDeSimbolos* new_TabDeSimbolos(const char* nome_inicial) {
-    TabDeSimbolos* nova_tab = (TabDeSimbolos*) malloc(sizeof(TabDeSimbolos));
+// Marca o último endereço de memória utilizado;
+static int endereco_atual = 0;
+static char TDS_saida_filename[] = "TabelasDeSimbolos.txt";
 
-    if (nova_tab == NULL) {
+// Gera um hash para alocar entradas na tabela de símbolos;
+// Na prática é uma função de hash simples que usa o algoritmo djb2;
+static unsigned int get_hash(const char *key)
+{
+    unsigned long hash_value = 5381;
+    int c;
+    // Enquanto houver caracteres na chave, calcula o hash;
+    while ((c = *key++))
+    {
+        // hash * 33 + c
+        hash_value = ((hash_value << 5) + hash_value) + c;
+    }
+    return hash_value % MAX_ENTRADAS;
+}
+
+const char* tipo_para_string(TipoDado tipo) {
+    switch (tipo) {
+        case INT:   return "INT";
+        case FLOAT: return "FLOATt";
+        case CHAR:  return "CHAR";
+        case STRUCT:return "STRUCT";
+        default:    return "??????";
+    }
+}
+
+// Retorna uma nova tabela de símbolos;
+TabDeSimbolos *new_TabDeSimbolos()
+{
+    // Aloca memória para a estrutura da tabela
+    TabDeSimbolos *tds = (TabDeSimbolos *)malloc(sizeof(TabDeSimbolos));
+    if (tds == NULL)
+    {
         perror("Falha ao alocar memória para TabDeSimbolos");
         return NULL;
     }
 
-    strncpy(nova_tab->nome, nome_inicial, sizeof(nova_tab->nome) - 1);
-    nova_tab->nome[sizeof(nova_tab->nome) - 1] = '\0';
-
-    return nova_tab;
-}
-
-void del_TabDeSimbolos(TabDeSimbolos* tab) {
-    if (tab != NULL) {
-        free(tab);
+    // Incilmente, todas as posições sao vazias;
+    for (int i = 0; i < MAX_ENTRADAS; i++)
+    {
+        tds->tabela[i] = NULL;
     }
+    return tds;
 }
 
-void printNome(const TabDeSimbolos *tab)
+// Deleta uma tabela de símbolos;
+void delete_TabDeSimbolos(TabDeSimbolos *tds)
 {
-    printf("%s\n", tab->nome);
+    if (tds == NULL)
+        return;
+
+    for (int i = 0; i < MAX_ENTRADAS; i++)
+    {
+        EntradaTDS *entradaAtual = tds->tabela[i];
+
+        // Libera nós e colisões;
+        while (entradaAtual != NULL)
+        {
+            EntradaTDS *temp = entradaAtual;
+            entradaAtual = entradaAtual->proximo;
+
+            free(temp->lexema);
+
+            free(temp);
+        }
+    }
+    // Destrói a tabela de símbolos;
+    free(tds);
+}
+
+// Insere um novo símbolo na tabela de símbolos;
+EntradaTDS *TDS_novoSimbolo(TabDeSimbolos *tds, const char *lexema, TipoDado tipo)
+{
+    // Encontra a posição do ssímbolo;
+    unsigned int i = get_hash(lexema);
+
+    // Caso o símbolo já esteja presente, evita a redeclaração;
+    EntradaTDS *atual = tds->tabela[i];
+    while (atual != NULL)
+    {
+        if (strcmp(atual->lexema, lexema) == 0)
+        {
+#warning "Erro semântico: Símbolo já declarado";
+            return atual;
+        }
+        atual = atual->proximo;
+    }
+
+    // Símbolo não encontrado, criamos uma nova entrada;
+    EntradaTDS *novaEntrada = (EntradaTDS *)malloc(sizeof(EntradaTDS));
+    if (novaEntrada == NULL)
+    {
+        perror("Falha ao alocar memória para Entrada da tabela de símbolos.");
+        return NULL;
+    }
+
+    // Configura o novo símbolo inserido;
+    novaEntrada->lexema = strdup(lexema);
+    novaEntrada->tipo = tipo;
+#warning "Endereço de memória não está sendo calculado corretamente, deve ser ajustado para o tipo de dado correto.";
+    novaEntrada->endereco = endereco_atual++;
+
+    novaEntrada->proximo = tds->tabela[i];
+    tds->tabela[i] = novaEntrada;
+
+    return novaEntrada;
+}
+
+// Encontra um símbolo na tabela de símbolos pelo nome;
+EntradaTDS *TDS_encontrarSimbolo(const TabDeSimbolos *tds, const char *lexema)
+{
+    unsigned int i = get_hash(lexema);
+    EntradaTDS *atual = tds->tabela[i];
+
+    while (atual != NULL)
+    {
+        if (strcmp(atual->lexema, lexema) == 0)
+        {
+            return atual; // Encontramos o símbolo;
+        }
+        atual = atual->proximo;
+    }
+
+    return NULL; // Nada feito, feijoada;
+}
+
+// Imprime a tabela de símbolos;
+void TDS_imprimir(const TabDeSimbolos *st)
+{
+    FILE *saida = fopen(TDS_saida_filename, "a");
+    if (saida == NULL)
+    {
+        perror("Nao foi possivel abrir o arquivo de saida para a tabela de simbolos");
+        return;
+    }
+
+    fprintf(saida, "\n--- Tabelas de símbolos ---\n");
+    for (int i = 0; i < MAX_ENTRADAS; i++)
+    {
+        EntradaTDS *atual = st->tabela[i];
+        if (atual != NULL)
+        {
+            fprintf(saida, "Entrada[%3d]:\n", i);
+            while (atual != NULL)
+            {
+                // Formata tudo bonitinho;
+                fprintf(saida, "  -> { Nome: %-15s, Tipo: %-10s, Endr: %d }\n",
+                        atual->lexema, tipo_para_string(atual->tipo), atual->endereco);
+                atual = atual->proximo;
+            }
+        }
+    }
+    fprintf(saida, "-------------------------------------\n");
+
+    fclose(saida);
 }
--- c-language/TabDeSimbolos.h
+++ c-language/TabDeSimbolos.h
@@ -2,17 +2,46 @@
 #define SYMTABLE
 
 #include <stdio.h>
-#include <string.h>
+
+#define MAX_ENTRADAS 127
+
+typedef enum
+{ // Define os tipos de dados possíveis;
+    FLOAT,
+    INT,
+    CHAR,
+    STRUCT
+} TipoDado;
+
+typedef struct EntradaTDS EntradaTDS;
+
+struct EntradaTDS
+{ // Define uma entrada na tabela de símbolos;
+    char *lexema;
+    TipoDado tipo;
+    int endereco;
+    EntradaTDS *proximo; // Marca quem é a próxima entrada da tabela;
+};
 
 typedef struct
-{
-    char nome[10];
+{ // Define a tabela de símbolos;
+    EntradaTDS *tabela[MAX_ENTRADAS];
 } TabDeSimbolos;
 
-TabDeSimbolos* new_TabDeSimbolos(const char* nome_inicial);
 
-void del_TabDeSimbolos(TabDeSimbolos* tab);
+// Retorna uma nova tabela de símbolos;
+TabDeSimbolos* new_TabDeSimbolos();
 
-void printNome(const TabDeSimbolos *tab);
+// Deleta uma tabela de símbolos;
+void delete_TabDeSimbolos(TabDeSimbolos *tds);
+
+// Insere um novo símbolo na tabela de símbolos;
+EntradaTDS* TDS_novoSimbolo(TabDeSimbolos* tds, const char *lexema, TipoDado tipo);
+
+// Encontra um símbolo na tabela de símbolos pelo nome;
+EntradaTDS* TDS_encontrarSimbolo(const TabDeSimbolos* tds, const char *lexema);
+
+// Imprime a tabela de símbolos;
+void TDS_imprimir(const TabDeSimbolos* tds);
 
 #endif // TabDeSimbolos
